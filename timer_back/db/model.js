d1abdd7a6661e9f7a9114d810be8f0cb917c180e
liudapavlenko <pavlenko.liudmyla@lll.kpi.ua>
2024-06-06 13:39:32 +0300

    Initial commit

---
+++ timer_back/db/model.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+
+module.exports = function (sequelize) {
+	return sequelize.define(
+		"Users",
+		{
+			id: {
+				type: Sequelize.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+				allowNull: false,
+			},
+			username: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+				unique: true,
+			},
+			usersurname: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+				unique: true,
+			},
+			email: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+				unique: true,
+			},
+			gender: {
+				type: Sequelize.TEXT,
+				allowNull: true,
+			},
+			birthdate: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+			password: {
+				type: Sequelize.TEXT,
+				allowNull: false,
+			},
+		},
+		{
+			timestamps: false,
+		}
+	);
+};
